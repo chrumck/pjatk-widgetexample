502953226857193ae398cfb8ce73b53973424b6d
Tomasz Dobrowolski <tdobrowolski@geocomp.com>
2019-01-24 17:47:37 -0500

    Add background change

--- app/src/main/java/pl/edu/pja/smb/widgetexample/FirstWidget.java
+++ app/src/main/java/pl/edu/pja/smb/widgetexample/FirstWidget.java
@@ -3,10 +3,16 @@ package pl.edu.pja.smb.widgetexample;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
@@ -15,19 +21,6 @@ import android.widget.RemoteViews;
  * Implementation of App Widget functionality.
  */
 public class FirstWidget extends AppWidgetProvider {
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.first_widget);
-
-        Intent intent = new Intent();
-        intent.setAction("goToPjwstk");
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
-        views.setOnClickPendingIntent(R.id.button, pi);
-
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -58,7 +51,47 @@ public class FirstWidget extends AppWidgetProvider {
             browserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(browserIntent);
         }
+
+        if (intent.getAction().equals("setBackground")) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            int resId = preferences.getInt("background", R.drawable.photo2);
+            int nextResId = resId == R.drawable.photo1 ? R.drawable.photo2 : R.drawable.photo1;
+
+            Bitmap bm = BitmapFactory.decodeResource(context.getResources(), nextResId);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.first_widget);
+            views.setImageViewBitmap(R.id.imgView, bm);
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), getClass().getName());
+            int ids[] = appWidgetManager.getAppWidgetIds(thisAppWidget);
+            for (int appWidgetID : ids) appWidgetManager.updateAppWidget(appWidgetID, views);
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("background", nextResId);
+            editor.apply();
+        }
     }
 
+    private static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                        int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.first_widget);
+
+        Intent intent = new Intent("goToPjwstk");
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.goToPjwstkBtn, pi);
+
+        Intent intent2 = new Intent("setBackground");
+        PendingIntent pi2 = PendingIntent.getBroadcast(context, 0, intent2, 0);
+        views.setOnClickPendingIntent(R.id.setBackgroundBtn, pi2);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        int resId = preferences.getInt("background", R.drawable.photo1);
+        Bitmap bm = BitmapFactory.decodeResource(context.getResources(), resId);
+        views.setImageViewBitmap(R.id.imgView, bm);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
 }
 
